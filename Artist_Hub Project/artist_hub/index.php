c4213cb1ed11248a370e3706c6a226f1cb5e03b6
Gaurang Raval <129145863+GaurangRavalG@users.noreply.github.com>
2025-10-13 12:37:26 +0530

    commit

--- Artist_Hub Project/artist_hub/index.php
+++ Artist_Hub Project/artist_hub/index.php
@@ -5,38 +5,38 @@ echo "<h2>🎯 Gaurang API</h2><br>";
 // Users APIs
 echo "<h3>👤 Users APIs</h3>";
 echo "<ul>";
-echo "<li><a href='gaurang/users/register.php'>Register User</a></li>";
-echo "<li><a href='gaurang/users/login.php'>Login User</a></li>";
-echo "<li><a href='gaurang/users/view_all.php'>View All Users</a></li>";
-echo "<li><a href='gaurang/users/update.php'>Update User</a></li>";
-echo "<li><a href='gaurang/users/delete.php'>Delete User</a></li>";
+echo "<li><a href='gaurang/register.php'>Register User</a></li>";
+echo "<li><a href='gaurang/login.php'>Login User</a></li>";
+echo "<li><a href='gaurang/view_all.php'>View All Users</a></li>";
+echo "<li><a href='gaurang/update.php'>Update User</a></li>";
+echo "<li><a href='gaurang/delete.php'>Delete User</a></li>";
 echo "</ul><br>";
 
 // Categories APIs
 echo "<h3>📂 Artist Category APIs</h3>";
 echo "<ul>";
-echo "<li><a href='gaurang/artist_category/add_category.php'>Add Category</a></li>";
-echo "<li><a href='gaurang/artist_category/view_all.php'>View All Categories</a></li>";
-echo "<li><a href='gaurang/artist_category/update_category.php'>Update Category</a></li>";
-echo "<li><a href='gaurang/artist_category/delete_category.php'>Delete Category</a></li>";
+echo "<li><a href='gaurang/add_category.php'>Add Category</a></li>";
+echo "<li><a href='gaurang/view_all.php'>View All Categories</a></li>";
+echo "<li><a href='gaurang/update_category.php'>Update Category</a></li>";
+echo "<li><a href='gaurang/delete_category.php'>Delete Category</a></li>";
 echo "</ul><br>";
 
 // Bookings APIs
 echo "<h3>📅 Bookings APIs</h3>";
 echo "<ul>";
-echo "<li><a href='gaurang/bookings/book_artist.php'>Book Artist</a></li>";
-echo "<li><a href='gaurang/bookings/view_all.php'>View All Bookings</a></li>";
-echo "<li><a href='gaurang/bookings/update_booking.php'>Update Booking</a></li>";
-echo "<li><a href='gaurang/bookings/delete_booking.php'>Delete Booking</a></li>";
+echo "<li><a href='gaurang/book_artist.php'>Book Artist</a></li>";
+echo "<li><a href='gaurang/view_all.php'>View All Bookings</a></li>";
+echo "<li><a href='gaurang/update_booking.php'>Update Booking</a></li>";
+echo "<li><a href='gaurang/delete_booking.php'>Delete Booking</a></li>";
 echo "</ul><br>";
 
 // Reviews APIs
 echo "<h3>⭐ Reviews APIs</h3>";
 echo "<ul>";
-echo "<li><a href='gaurang/reviews/add_review.php'>Add Review</a></li>";
-echo "<li><a href='gaurang/reviews/view_all.php'>View All Reviews</a></li>";
-echo "<li><a href='gaurang/reviews/update_review.php'>Update Review</a></li>";
-echo "<li><a href='gaurang/reviews/delete_review.php'>Delete Review</a></li>";
+echo "<li><a href='gaurang/add_review.php'>Add Review</a></li>";
+echo "<li><a href='gaurang/view_all.php'>View All Reviews</a></li>";
+echo "<li><a href='gaurang/update_review.php'>Update Review</a></li>";
+echo "<li><a href='gaurang/delete_review.php'>Delete Review</a></li>";
 echo "</ul><br>";
 
 ?>
